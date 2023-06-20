4d2ada2936e029a90fa72d50b29d5b338e9adc61
RocioMartinez <rym5411@gmail.com>
2023-06-20 16:54:39 -0400

    initial set up of front end

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,14 +1,32 @@
 import React from 'react';
+import axios from 'axios';
+import { Carousel } from 'react-bootstrap';
+import './App.css';
 
 class BestBooks extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      books: []
+      books:[]
     }
   }
 
-  /* TODO: Make a GET request to your API to fetch all the books from the database  */
+  getAllBooks = async () => {
+    try {
+      let url = `${process.env.REACT_APP_SERVER}/books`
+
+      let booksFromDB = await axios.get(url);
+      this.setState({
+        books: booksFromDB.data
+      })
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
+  componentDidMount() {
+    this.getAllBooks();
+  }
 
   render() {
 
@@ -18,10 +36,26 @@ class BestBooks extends React.Component {
       <>
         <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
 
-        {this.state.books.length ? (
-          <p>Book Carousel coming soon</p>
+        {this.state.books.length > 0? (
+          <div className='carousel_container'>
+            <Carousel>
+              {this.state.books.map((book, index) => {
+                return <Carousel.Item key={index}>
+              <img
+                className="d-block w-100"
+                src={require(`./imgs/${book._id}.jpg`)}
+                alt={book.title}
+                />
+              <Carousel.Caption>
+                <h3>{book.title}</h3>
+                <p>{book.description}</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+    })}
+            </Carousel>
+          </div>
         ) : (
-          <h3>No Books Found :(</h3>
+          <h3>No Books Found :</h3>
         )}
       </>
     )
